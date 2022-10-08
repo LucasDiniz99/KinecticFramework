23d3fd6d212a68386e43b2c68feac8618cfe3022
Lucas Rodrigues Diniz <lucas_diniz99@hotmail.com.br>
2022-10-08 13:24:54 -0300

    added missing files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,36 @@
-{
-    test: /\.(sa|sc|c)ss$/,
-    use: [
-            {
-                loader: 'style-loader',
-                options: {
-                    hmr: true
-                }
-            },
-            {
-                loader: 'css-loader',
-                options: {
-                   modules: true,
-                   importLoaders: 1,
-                   localIdentName: '[name]__[local]___[hash:base64:5]',
-                },
-           },
-           {
-               loader: 'postcss-loader',
-               options: {
-                   plugins: [autoprefixer({ browsers: ['> 1%'] })]
-               },
-            },
-            'sass-loader'
-        ],
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    rules: [
+        {
+          test: /\.js$/,
+          /* ... */
+        },
+        {
+          // Apply rule for .sass, .scss or .css files
+          test: /\.(sa|sc|c)ss$/,
+    
+          // Set loaders to transform files.
+          // Loaders are applying from right to left(!)
+          // The first loader will be applied after others
+          use: [
+                 {
+                   // This loader resolves url() and @imports inside CSS
+                   loader: "css-loader",
+                 },
+                 {
+                   // Then we apply postCSS fixes like autoprefixer and minifying
+                   loader: "postcss-loader"
+                 },
+                 {
+                   // First we transform SASS to standard CSS
+                   loader: "sass-loader",
+                   options: {
+                     implementation: require("sass")
+                   }
+                 }
+               ]
+        }
+    ]
 }
